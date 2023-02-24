9e8c28a51b3dff6c0aa6fb5ced3819f2d4a406cb
unknown <waismalikzada9@gmail.com>
2023-02-24 21:44:23 +0430

    add designs to view

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,9 +1,18 @@
+@extends('layout')
+
+@section('content')
+@include('partials._hero')
+@include('partials._search')
 <h1>{{$headers}}</h1>
+<div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+@unless (count($listings) == 0)
+    
 @foreach ($listings as $list)
-    <h2>
-        <a href="/listing/{{$list['id']}}">
-            {{$list['title']}}
-        </a>
-    </h2>
-    <p>{{$list['description']}}</p>
+    <x-listing-card :list="$list"/>
 @endforeach
+@else
+<p>No listings found!</p>
+@endunless    
+
+</div>
+@endsection
